df7f1c7d1bef0917d827b96e45ee7a3590967f15
Jesseract <jlpearce@gmail.com>
2015-03-30 11:10:17 -0400

    Styling fixes

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,13 @@
 
 ActiveRecord::Schema.define(version: 20150329172349) do
 
+  create_table "albums", force: :cascade do |t|
+    t.text     "url"
+    t.integer  "photo_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "landmarks", force: :cascade do |t|
     t.string   "name"
     t.text     "link"
